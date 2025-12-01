0844a526c334c82c50d7993ef85fade5702cb8f1
mmahadi-ahmedd <mahadiahmed13550@gmail.com>
2025-12-01 20:57:58 +0600

    primarily added student components

---
+++ src/Components/Student/CoursesSection/CoursesSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CoursesSection = () => {
+    return (
+        <div>
+            <h1>Courses SEction</h1>
+        </div>
+    );
+};
+
+export default CoursesSection;
