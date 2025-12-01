7554d24ea6f38c0cd81fbaee495a132c052558fc
mmahadi-ahmedd <mahadiahmed13550@gmail.com>
2025-12-01 23:35:39 +0600

    created routes for student

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/favicon.svg'
-import './App.css'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Student/Home/Home';
+import CoursesList from './Pages/Student/CoursesList/CoursesList';
+import CourseDetailsPage from './Pages/Student/CourseDetailsPage/CourseDetailsPage';
+import MyEnrollments from './Pages/Student/MyEnrollments/MyEnrollments';
+import Player from './Pages/Student/Player/Player';
+import Loading from './Components/Student/Loading/Loading';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Routes>
+        <Route  path='/' element= {<Home/>} />
+        <Route  path='/courseList' element={<CoursesList/>} />
+        <Route  path='/courseList/:input' element={<CoursesList/>} />
+        <Route  path='/course/:id' element={<CourseDetailsPage/>} />
+        <Route  path='/myEnrollments' element={<MyEnrollments/>} />
+        <Route  path='/player/:courseId' element={<Player/>} />
+        <Route  path='/loading/:path' element={<Loading/>} />
+        
+        </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {  RouterProvider } from "react-router";
-import { router } from './Router/Router.jsx';
+import { AppContextProvider } from './Context/AppContext.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-   <RouterProvider router={router} />
-  </StrictMode>,
+  <BrowserRouter>
+  <AppContextProvider>
+   <App/>
+  </AppContextProvider>
+  </BrowserRouter>,
 )
