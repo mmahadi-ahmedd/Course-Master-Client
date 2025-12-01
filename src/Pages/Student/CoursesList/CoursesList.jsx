cdb24ea7e771033a83dcd29d03b9cc637836d58a
mmahadi-ahmedd <mahadiahmed13550@gmail.com>
2025-12-01 21:26:10 +0600

    primarily created student page

---
+++ src/Pages/Student/CoursesList/CoursesList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CoursesList = () => {
+    return (
+        <div>
+            <h1>Courses list</h1>
+        </div>
+    );
+};
+
+export default CoursesList;
---
+++ src/Pages/Student/Player/Player.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Player = () => {
+    return (
+        <div>
+            <h1>Playrer</h1>
+        </div>
+    );
+};
+
+export default Player;
