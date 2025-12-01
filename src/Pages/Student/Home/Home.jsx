0b0afb6e290746f3440bcfdfd564e3cc85abae9f
mmahadi-ahmedd <mahadiahmed13550@gmail.com>
2025-12-02 03:12:56 +0600

    created hero section

--- src/Pages/Student/Home/Home.jsx
+++ src/Pages/Student/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import Hero from '../../../Components/Student/Hero/Hero';
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <div className='flex flex-col items-center space-y-7 text-center' >
+          <Hero/>
+        </div>
         </div>
     );
 };
