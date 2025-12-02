d6b836e910514d45ae2d58e4686816fad0e5ab51
mmahadi-ahmedd <mahadiahmed13550@gmail.com>
2025-12-02 12:33:20 +0600

    added companies

--- src/Pages/Student/Home/Home.jsx
+++ src/Pages/Student/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Hero from '../../../Components/Student/Hero/Hero';
+import Companies from '../../../Components/Student/Companies/Companies';
 const Home = () => {
     return (
         <div>
             <div className='flex flex-col items-center space-y-7 text-center' >
-          <Hero/>
-        </div>
+                <Hero />
+                <Companies/>
+            </div>
         </div>
     );
 };
